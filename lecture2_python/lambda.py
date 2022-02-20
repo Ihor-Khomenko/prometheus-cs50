11af8c59de4b7ed1ff2a52b9ba640ebecbfb1175
Ihor Khomenko <9989281+Ihor-Khomenko@users.noreply.github.com>
2022-02-20 22:52:27 +0200

    lambda, exceptions

---
+++ lecture2_python/lambda.py
@@ -0,0 +1,17 @@
+people = [
+    {"name": "Harry", "house": "Griffindor"},
+    {"name": "Cho", "house": "Ravenclaw"},
+    {"name": "Draco", "house": "Slytherin"}
+]
+
+#def f(person):
+#    return person["name"]
+
+#people.sort(key=f)
+
+print(people)
+
+# lambda
+
+people.sort(key=lambda person: person["name"])
+print(people)
